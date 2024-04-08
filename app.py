d5eebb40e09db382e2a77deaf196d56a15333795
Daniel-pogi <danielcaraan9@gmail.com>
2024-04-08 14:51:55 +0800

    connect database

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+class Note(db.Model):
+    id = Column(Integer, primary_key=True)
+    data = Column(String, nullable=False)
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db.init_app(app)
 
 @app.route('/')
 @app.route('/home')
